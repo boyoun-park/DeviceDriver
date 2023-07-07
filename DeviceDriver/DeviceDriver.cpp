32a532086af0287becf016cfc807cffc84733dc0
boyoun_park <boyounpark2019@gmail.com>
2023-07-07 11:05:22 +0900

    [feat] add read for device driver

--- DeviceDriver/DeviceDriver.cpp
+++ DeviceDriver/DeviceDriver.cpp
@@ -5,8 +5,13 @@ DeviceDriver::DeviceDriver(FlashMemoryDevice *hardware) : m_hardware(hardware)
 
 int DeviceDriver::read(long address)
 {
-	// TODO: implement this method properly
-	return (int)(m_hardware->read(address));
+	int prevValue = m_hardware->read(address);
+	int nextValue;
+	for (int tryRead = 0; tryRead < 4; tryRead++){
+		nextValue = m_hardware->read(address);
+		if (prevValue != nextValue) throw(ReadFailException());
+	}
+	return (int)(prevValue);
 }
 
 void DeviceDriver::write(long address, int data)
--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -1,7 +1,23 @@
 #include "gtest/gtest.h"
 #include "gmock/gmock.h"
+#include "../DeviceDriver/DeviceDriver.cpp"
+
+using namespace testing;
+
+class MockDevice : public FlashMemoryDevice {
+public:
+	MOCK_METHOD(unsigned char, read, (long address), (override));
+	MOCK_METHOD(void, write, (long address, unsigned char data), (override));
+};
 
 TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+	MockDevice mock;
+	DeviceDriver driver(&mock);
+
+	EXPECT_CALL(mock, read(0x1))
+		.WillOnce(Return(0x1))
+		.WillOnce(Return(0x2))
+		.WillRepeatedly(Return(0x1));
+
+	EXPECT_THROW(driver.read(0x1), ReadFailException);
 }
